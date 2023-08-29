142dbd6e39134168784d89f8ee4bf6525c1c5aaf
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-08-29 15:01:20 +0300

    up date

--- src/app/CustomDirective/betterhighlight.directive.ts.directive.ts
+++ src/app/CustomDirective/betterhighlight.directive.ts.directive.ts
@@ -1,11 +1,21 @@
-import { Directive, ElementRef, HostBinding, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterhighlightDirectiveTs]'
 })
 export class BetterhighlightDirectiveTsDirective {
 
-  constructor(private element:ElementRef, private renderer:Renderer2 ) { }
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
 
   @HostBinding('style.backgroundColor') background: string = 'transparent'
+
+
+  @HostListener('mouseenter') mouseenter() {
+    this.background = 'yellow'
+  }
+
+  @HostListener('mouseleave') mouseleave() {
+    this.background = 'pink'
+  }
+
 }
