c5c5fdcbecf737c7596a32924b39a3f1ee9ff096
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-08-29 12:07:49 +0300

    UP date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SetBackGroundDirective } from '../app/CustomDirective/setBackGround.directive'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetBackGroundDirective
   ],
   imports: [
     BrowserModule,
